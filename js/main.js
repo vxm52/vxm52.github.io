0c5816b140249fc15793735066b851a81a0fdf44
vxm52 <vaibhav.s.malla@gmail.com>
2020-08-30 02:30:05 -0400

    Added Skills section and minor tweaks

--- js/main.js
+++ js/main.js
@@ -85,26 +85,26 @@ $(document).ready(function() {
   $('.project-area .grid .test-popup-link').magnificPopup({
   type: 'image',
   gallery:{enabled:true}
-
   });
 
-
-  // Sticky navigation menu
-
-  let nav_offset_top = $('.header_area').height() + 600;
-
-  function navbarFixed() {
-    if ($('.header_area').length) {
-      $(window).scroll(function() {
-        let scroll = $(window).scrollTop();
-        if (scroll >= nav_offset_top) {
-          $('.header_area .main-menu').addClass('navbar_fixed');
-        } else {
-          $('.header_area .main-menu').removeClass('navbar_fixed');
-        }
-      })
-    }
-  }
-
-  navbarFixed();
 });
+
+
+// Sticky navigation menu
+
+let nav_offset_top = $('.header_area').height() + 600;
+
+function navbarFixed() {
+  if ($('.header_area').length) {
+    $(window).scroll(function() {
+      let scroll = $(window).scrollTop();
+      if (scroll >= nav_offset_top) {
+        $('.header_area .main-menu').addClass('navbar_fixed');
+      } else {
+        $('.header_area .main-menu').removeClass('navbar_fixed');
+      }
+    })
+  }
+}
+
+navbarFixed();

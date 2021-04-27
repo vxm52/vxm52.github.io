4d536b83b2bb9879b0932ee2f12bad187e97d226
vxm52 <vaibhav.s.malla@gmail.com>
2021-04-27 15:29:40 -0400

    Added waving hand emoji and updated Projects section

--- js/main.js
+++ js/main.js
@@ -70,18 +70,20 @@ $(document).ready(function() {
   $btns.click(function(e) {
     $('.project-area .button-group button').removeClass('active');
     e.target.classList.add('active');
+    
     let selector = $(e.target).attr('data-filter');
     $('.project-area .grid').isotope({
       filter : selector
     });
+
     return false;
-  })
+  });
+  
   $('.project-area .button-group #btn1').trigger('click');
   $('.project-area .grid .test-popup-link').magnificPopup({
-  type: 'image',
-  gallery:{enabled:true}
+    type: 'image',
+    gallery:{enabled:true}
   });
-
 
 
   // Collapsed Navbar close-on-click outside
